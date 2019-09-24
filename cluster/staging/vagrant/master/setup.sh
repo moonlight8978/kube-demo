738a334fc942cb142d1dc04cd8967964fd258dd7
_MoonLight_ <moonlight8978@gmail.com>
2019-09-24 13:58:05 +0700

    vagrant

---
+++ cluster/staging/vagrant/master/setup.sh
@@ -0,0 +1,75 @@
+sudo su -
+yum update -y
+
+# Install required packages.
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Add Docker repository.
+yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker CE.
+yum update -y && yum install -y docker-ce-18.09.9
+
+# Create /etc/docker directory.
+mkdir /etc/docker
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker
+systemctl daemon-reload
+systemctl restart docker
+
+# Enable docker service
+systemctl enable docker.service
+
+# Fix bug that iptables being bypassed
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+# Add k8s repo
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Install k8s base
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+# Enable kubelet
+systemctl enable --now kubelet
+
+# Turn off swap
+swapoff -a
+
+# Init k8s cluster
+kubeadm config images pull
+kubeadm init
