785af025721656fae78a8809eb25bc0f137b995b
_MoonLight_ <moonlight8978@gmail.com>
2019-10-10 08:37:04 +0700

    test volume

--- cluster/staging/glusterfs/setup.sh
+++ cluster/staging/glusterfs/setup.sh
@@ -1,6 +1,50 @@
+# On gluster nodes
 yum update -y
 
 yum install centos-release-gluster -y
-yum install glusterfs gluster-cli glusterfs-libs glusterfs-server -y
+yum install glusterfs-server -y
+
+# Create partition
+wipefs /dev/sdb -a
+fdisk /dev/sdb # Following by n-p-1-t-8e-p-w
+
+# Create LV using LVM
+pvcreate /dev/sdb1
+vgcreate vg_gluster /dev/sdb1
+lvcreate -L 5G -n database1 vg_gluster
+
+# Format the LV using xfs
+mkfs.xfs /dev/vg_gluster/database1
+
+# Mount LV
+mkdir -p /data/glusterfs/database/brick1
+mount /dev/vg_gluster/database1 /data/glusterfs/database/brick1
+
+# Persist mount on booting
+echo '/dev/vg_gluster/database1 /data/glusterfs/database/brick1 xfs defaults 0 0' >> /etc/fstab
+
+# Start glusterfs systemd
+systemctl enable glusterd
+systemctl start glusterd
+
+# Add peer
+gluster peer probe gluster-2
+
+# Create volume
+gluster volume create database replica 2 gluster-1:/data/glusterfs/database/brick1 gluster-2:/data/glusterfs/database/brick2
+
+# Start volume
+gluster volume start database
+
+##############################
+
+# On worker node
+yum update -y
+
+yum install glusterfs-fuse -y
+
+# By default, SELinux does not allow writing from a pod to a remote GlusterFS server
+setsebool -P virt_sandbox_use_fusefs on
+setsebool -P virt_use_fusefs on
 
 
