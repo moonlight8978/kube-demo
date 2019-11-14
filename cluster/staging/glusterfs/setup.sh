1444be0040b25c14a9399eb60a94a530b2165136
Lê Sĩ Bích <moonlight8978@gmail.com>
2019-11-14 11:12:19 +0700

    Use k8s to deploy staging (#3)
    * config
    
    * config
    
    * config
    
    * vagrant
    
    * refactor vagrant config
    
    * token
    
    * update
    
    * traefik
    
    * fix deployment apiVersion
    
    * fix deployment selector
    
    * add namespace resource
    
    * change command to args
    
    * nodeport
    
    * args
    
    * healthcheck
    
    * revert
    
    * args
    
    * fix network
    
    * fix namespace
    
    * traefik
    
    * fix
    
    * rename namespace
    
    * fixed node port, remove server in ingress
    
    * vagrant file, persistent volume config
    
    * fix apiVersion;
    
    * database
    
    * rename endpoint
    
    * fix port
    
    * test endpoints
    
    * glusterfs script
    
    * test volume
    
    * fix ip
    
    * fix typo
    
    * fix
    
    * fix
    
    * remove headless service
    
    * storageos
    
    * cluster storage
    
    * storageos claim
    
    * fix glusterfs
    
    * fix storageclassname
    
    * fix gluster config
    
    
    Co-authored-by: MinhNgoc <minhngoc@MinhNgocs-MacBook-Air.local>

---
+++ cluster/staging/glusterfs/setup.sh
@@ -0,0 +1,54 @@
+# On gluster nodes
+yum update -y
+
+yum search centos-release-gluster
+yum install centos-release-gluster6.noarch -y
+yum install glusterfs-server -y
+
+# Create partition
+wipefs /dev/sdb -a
+fdisk /dev/sdb # Following by n-p-1-t-8e-p-w
+
+# Create LV using LVM
+pvcreate /dev/sdb1
+vgcreate vg_gluster /dev/sdb1
+lvcreate -L 2G -n database1 vg_gluster
+
+# Format the LV using xfs
+mkfs.xfs /dev/vg_gluster/database1
+
+# Mount LV
+mkdir -p /data/glusterfs/database/brick1
+mount /dev/vg_gluster/database1 /data/glusterfs/database/brick1
+
+# Persist mount on booting
+echo '/dev/vg_gluster/database1 /data/glusterfs/database/brick1 xfs defaults 0 0' >> /etc/fstab
+
+# Start glusterfs systemd
+systemctl enable glusterd
+systemctl start glusterd
+
+# Add peer
+gluster peer probe gluster-2
+
+# Create volume
+gluster volume create database replica 2 gluster-1:/data/glusterfs/database/brick1/brick gluster-2:/data/glusterfs/database/brick2/brick
+
+# Start volume
+gluster volume start database
+
+##############################
+
+# On worker node
+yum update -y
+
+yum search centos-release-gluster
+yum install centos-release-gluster6.noarch -y
+yum install glusterfs-fuse -y
+
+# Enable fuse kernel module
+modprobe fuse
+
+# By default, SELinux does not allow writing from a pod to a remote GlusterFS server
+setsebool -P virt_sandbox_use_fusefs on
+setsebool -P virt_use_fusefs on
