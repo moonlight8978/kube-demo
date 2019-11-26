b24a8899cce5ec9f4364863a33887b97bf8e08ac
_MoonLight_ <moonlight8978@gmail.com>
2019-11-26 08:23:51 +0700

    config

--- web/config/initializers/sidekiq.rb
+++ web/config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}" }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}" }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
