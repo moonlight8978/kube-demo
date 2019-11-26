a174a7d921ff3013bf08c7bffb7427fb252c95e8
Le Si Bich <le.si.bich@sun-asterisk.com>
2019-11-26 15:01:14 +0700

    staging config

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -28,8 +28,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['ASSET_HOST'] if ENV['ASSET_HOST'].present?
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
